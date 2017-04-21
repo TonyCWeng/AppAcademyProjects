8f3f89942bed3000ce94c8e033943d42ca8a560f
App Academy Student <student@appacademy.io>
2017-04-21 16:48:20 -0700

    finish capybara specs for users

--- user_tests/app/models/user.rb
+++ user_tests/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, :password, :password_digest, :session_token, presence: true
+  validates :email, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
   attr_reader :password
   after_initialize :ensure_session_token
@@ -29,8 +29,10 @@ class User < ApplicationRecord
     self.session_token ||= User.generate_session_token
   end
 
-  def reset_sesssion_token!
+  def reset_session_token!
     self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
   end
 
 end
--- user_tests/spec/models/user_spec.rb
+++ user_tests/spec/models/user_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe User, type: :model do
 
   describe 'Validates' do
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password) }
     it { should validate_presence_of(:session_token)}
     it { should validate_presence_of(:password_digest) }
     it { should validate_length_of(:password).is_at_least(6) }
