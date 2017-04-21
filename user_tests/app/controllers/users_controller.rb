917013968e41fb53e27df50bc9c13b84b4ad0dea
Tony Weng <tcweng91@gmail.com>
2017-04-21 14:41:32 -0700

    finish user controller

--- user_tests/app/controllers/users_controller.rb
+++ user_tests/app/controllers/users_controller.rb
@@ -1,3 +1,45 @@
 class UsersController < ApplicationController
-  
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = ["Invalid username/password"]
+      redirect_to new_user_url
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    redirect_to users_url
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user
+      render :show
+    else
+      flash[:errors] = ["User not found!"]
+      redirect_to users_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
--- user_tests/spec/controllers/users_controller_spec.rb
+++ user_tests/spec/controllers/users_controller_spec.rb
@@ -2,31 +2,67 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  subject(:user) { User.new(email: 'email', password: 'password') }
+
   describe 'GET #index' do
-    it 'renders the users index page'
+    it 'renders the users index page' do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 
   describe 'GET #show' do
+
     context "valid user id" do
-      it 'renders a specific user page'
+      it 'renders a specific user page' do
+        user.save!
+        get :show, params: { id: user.id }
+        expect(response).to render_template("show")
+      end
     end
 
     context "invalid user id" do
-      it 'redirects to the users index page'
+      it 'redirects to the users index page' do
+        get :show, params: { id: -1 }
+        expect(response).to redirect_to "#{users_url}"
+        expect(flash[:errors]).to be_present
+      end
     end
   end
 
   describe 'GET #new' do
-    it 'should render a user create form'
+    it 'should render a user create form' do
+      get :new
+      expect(response).to render_template("new")
+    end
   end
 
   describe 'DELETE #destroy' do
-    it 'should delete a user from the database'
+    it 'should delete a user from the database' do
+      user.save!
+      expect(User.all.length).to eq(1)
+
+      delete :destroy, params: { id: user.id }
+      expect(response).to redirect_to(users_url)
+      expect(User.all.length).to eq(0)
+    end
   end
 
   describe 'POST #create' do
-    it 'should create a new user'
-    it 'should redirect to a specific user show page'
+    context "with valid params" do
+      it 'should redirect user to users show page on success' do
+        post :create, params: { user: {email: "something@gmail.com", password: "password"} }
+        expect(response).to redirect_to(user_url(User.find_by(email: "something@gmail.com")))
+      end
+
+    end
+
+    context "with invalid params" do
+      it 'should redirect_to registration page' do
+        post :create, params: { user: {email: "", password: ""} }
+        expect(response).to redirect_to(new_user_url)
+      end
+    end
   end
 
 end
