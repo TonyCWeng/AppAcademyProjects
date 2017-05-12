c01233b0ca0697605d1f81793d66a3c0a083ce69
App Academy Student <student@appacademy.io>
2017-05-12 14:28:08 -0700

    Complete session util

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
-  validates :password, length: {minimum: 6, allow_nil: true }
+  validates :password, length: { minimum: 6, allow_nil: true }
   before_validation :ensure_session_token
   attr_reader :password
 
